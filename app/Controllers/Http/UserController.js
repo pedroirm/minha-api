1e8758dd7d66f909d21b1c163e69ea6e2ed254b3
Pedro Icaro <pedroicaro35@gmail.com>
2021-02-13 13:54:36 -0300

    First Commit

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,30 @@
+'use strict'
+const User = use('App/Models/User')
+const { validateAll } = use('Validator')
+class UserController {
+    async create({ request, response }) {
+        try {
+            const erroMessage = {
+                'username.required': 'Esse campo é requerido'
+            }
+            const validation = await validateAll(request.all(), {
+                username: 'required|min:3|unique:users',
+                email: 'required|email|unique:users',
+                password: 'required|min:6'
+            })
+            if (validation.fails()) {
+                return response.status(401).send({ message: validation.messages() })
+            }
+            const data = request.only(['username', 'email', 'password'])
+
+            const user = await User.create(data)
+
+            return user
+
+        } catch (error) {
+            return response.status(500).send({ error: `Erro: ${error}` })
+        }
+    }
+}
+
+module.exports = UserController
